c8fbba6a64b6ff0a2cc586310eaab08880ce436b
Szustarol <szustkarol@gmail.com>
2022-05-14 14:33:14 +0200

    Home screen css

--- client/src/screen/HomeScreen.tsx
+++ client/src/screen/HomeScreen.tsx
@@ -1,9 +1,40 @@
 import React, { useContext } from 'react';
 import { MapContext } from '../context/Context';
+import './HomeScreen.css'
 
 function HomeScreen() {
   return (
-    <div>
+    <div className='home-screen-container'>
+      <div className='title-container'>
+      【~~　ＶａｐｏｒＷＩＥＴ　~~】
+      </div>
+      <a href='#' className='no-decor'>
+        <div className='create-room-button'>
+          <div className='create-room-inner'>
+            [１]　Ｃｒｅａｔｅ　ｒｏｏｍ
+          </div>
+        </div>
+      </a>
+      <div className='room-id-hint-container'>
+      【Ｏｒ　ｉｎｓｅｒｔ　ｒｏｏｍ　ｉｄ】
+      </div>
+      <div className='room-id-container'>
+        <div className='input-spacer'>
+        </div>
+        <input className='room-id-input' placeholder='Type here ...' type='text'></input>
+        <div className='input-reset-container'>
+          <a className='no-decor'>
+            <div className='input-reset'>
+
+            </div>
+          </a>
+        </div>
+        <div className='input-spacer'>
+        </div>
+      </div>
+      <br/>
+      &nbsp;
+      <br/>
     </div>
   );
 }
