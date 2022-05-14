2f94bf971728af2a7750187cd05984db17a6b595
Kamil Delekta <dkamil17@wp.pl>
2022-05-14 11:13:46 +0200

    Adding context v2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,27 @@
 import { useState } from 'react';
 import './App.css';
-import { BrowserRouter, Route,Routes } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import HomeScreen from './screen/HomeScreen';
+import MapProvider from './context/Context';
+import Board from './screen/Board';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
+      <MapProvider>
         <Header />
-            <Routes>
-              <Route path="/" element={<HomeScreen />} />
-            </Routes>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/home" element={<HomeScreen />} />
+          <Route path="/board" element={<Board />} />
+        </Routes>
         {/* <Footer /> */}
-      </BrowserRouter>
+      </MapProvider>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,29 +1,38 @@
-import React from 'react'
-import './Header.css'
-import navbarIcon from '../assets/images/navbarIcon.png'
+import React from 'react';
+import './Header.css';
+import navbarIcon from '../assets/images/navbarIcon.png';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav } from 'react-bootstrap';
 
 function Header() {
   return (
     <header>
-        <div className='header'>
-            <div className='container'>
-                <img src={navbarIcon} className='navbarIcon'></img>
-                <LinkContainer to="/">
-                    <Navbar.Brand><div className='container appTitle'>【~~ＶａｐｏｒＷＩＥＴ~~】</div></Navbar.Brand>
-                </LinkContainer>
-            </div>
-            <div>
-            <Nav className="me-auto">
-                <LinkContainer to="/home">
-                    <Nav.Link><div className='container'>【Ｈｏｍｅ】</div></Nav.Link>
-                </LinkContainer>
-            </Nav>
-            </div>
+      <div className="header">
+        <div className="container">
+          <img src={navbarIcon} className="navbarIcon"></img>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <div className="container appTitle">【~~ＶａｐｏｒＷＩＥＴ~~】</div>
+            </Navbar.Brand>
+          </LinkContainer>
         </div>
+        <div>
+          <Nav className="me-auto">
+            <LinkContainer to="/home">
+              <Nav.Link>
+                <div className="container">【Ｈｏｍｅ】</div>
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/board">
+              <Nav.Link>
+                <div className="container">【Board】</div>
+              </Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </div>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
