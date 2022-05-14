87bb432e2e04c1731c33129708936f1864cfe436
Kamil Delekta <dkamil17@wp.pl>
2022-05-14 14:31:07 +0200

    Add configuration->board navigation

--- client/src/screen/BoardScreen.tsx
+++ client/src/screen/BoardScreen.tsx
@@ -1,26 +1,37 @@
 import React, { useContext, useEffect } from 'react';
 import { MapContext } from '../context/Context';
-import Board from '../components/Board'
+import Board from '../components/Board';
 import { mapStateMock } from '../types/BoardTypes';
-import './BoardScreen.css'
+import './BoardScreen.css';
 import PlayerStats from '../components/Statistics/PlayerStats';
+import { useLocation } from 'react-router-dom';
+
+export interface BoardProps {
+  width: number;
+  height: number;
+  maxPoints: number;
+  time: number;
+}
 
 function BoardScreen() {
-  const {setMapState} = useContext(MapContext)
+  const location = useLocation();
+  const props: BoardProps = location.state;
+
+  const { setMapState } = useContext(MapContext);
   return (
-    <div className='board-container'>
+    <div className="board-container">
       <PlayerStats />
-      <div className='grey-border'>
-        <div className='white-border'>
-          <div className='grey-border'>
-            <div className='violet-border'>
-              <Board width={10} height={10}/>
+      <div className="grey-border">
+        <div className="white-border">
+          <div className="grey-border">
+            <div className="violet-border">
+              <Board width={props.width} height={props.height} />
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default BoardScreen;
