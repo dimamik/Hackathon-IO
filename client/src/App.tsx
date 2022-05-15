fb39aebb017766de1ec2e96ca0bd05c09017fdf6
Szustarol <szustkarol@gmail.com>
2022-05-15 12:10:02 +0200

    Fixed app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,27 +8,27 @@ import BoardScreen from './screens/Board/BoardScreen';
 import LocalBoardScreen from './screens/Board/LocalBoardScreen';
 import ConfigurationScreen from './screens/Configuration/ConfigurationScreen';
 import QuestionComponents from './components/Modals/QuestionComponents';
-import AdditionalQuestionScreen from './screens/AdditionalQuestion/AdditionalQuestionScreen';
+import GameOverComponts from './components/Modals/GameOverComponts';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <BrowserRouter>
-
-      <MapProvider>
-        <Header />
-        <div className="mainBackground">
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/home" element={<HomeScreen />} />
-            <Route path="/board" element={<BoardScreen />} />
-            <Route path="/configuration" element={<ConfigurationScreen />} />
-            <Route path="/question" element={<QuestionComponents />} />
-            <Route path="/add" element={<AdditionalQuestionScreen />} />
-          </Routes>
-          {/* <Footer /> */}
-        </div>
-
+      <MapProvider contextInstance={MapContext}>
+        <MapProvider contextInstance={SecondPlayerMapContext}>
+          <Header />
+          <div className="mainBackground">
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/home" element={<HomeScreen />} />
+              <Route path="/board" element={<BoardScreen />} />
+              <Route path="/boardLocal" element={<LocalBoardScreen />} />
+              <Route path="/configuration" element={<ConfigurationScreen />} />
+              <Route path="/question" element={<QuestionComponents />} />
+            </Routes>
+            {/* <Footer /> */}
+          </div>
+        </MapProvider>
       </MapProvider>
     </BrowserRouter>
   );
